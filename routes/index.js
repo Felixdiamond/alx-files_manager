5a3af7de3846c0864e709f91e3add02e9402c0e8
Felixdiamond <diamondfelix006@gmail.com>
2023-09-05 09:28:08 -0700

    Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', (req, res) => AppController.getStatus(req, res));
 router.get('/stats', (req, res) => AppController.getStats(req, res));
+router.post('/users', (req, res) => UsersController.postNew(req, res));
 
 export default router;

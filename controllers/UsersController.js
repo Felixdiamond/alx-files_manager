96ba28ceaa4a2eca4098cc1bc47714b6e5bdadd2
Felixdiamond <diamondfelix006@gmail.com>
2023-09-05 09:29:55 -0700

    Create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
+
+    const emailExists = await dbClient.users.findOne({ email });
+    if (emailExists) return res.status(400).send({ error: 'Already exist' });
+
+    const hashedPassword = sha1(password);
+    const newUser = await dbClient.users.insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).send({
+      id: newUser.insertedId,
+      email,
+    });
+  }
+}
+
+export default UsersController;

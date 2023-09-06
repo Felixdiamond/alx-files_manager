cf5c64b42a10ec568af6149da4e08005cf988d34
Felixdiamond <diamondfelix006@gmail.com>
2023-09-06 04:21:34 -0700

    First file

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
-const FilesController = require('../controllers/FilesController');
+import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
 
 const router = express.Router();

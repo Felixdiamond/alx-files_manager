fa45f666d96bc0f0a2436e62070b30f41fe38b42
Felixdiamond <diamondfelix006@gmail.com>
2023-09-05 17:55:39 -0700

    First file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
+const FilesController = require('../controllers/FilesController');
 import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
@@ -11,5 +12,6 @@ router.post('/users', (req, res) => UsersController.postNew(req, res));
 router.get('/connect', (req, res) => AuthController.getConnect(req, res));
 router.get('/disconnect', (req, res) => AuthController.getDisconnect(req, res));
 router.get('/users/me', (req, res) => UsersController.getMe(req, res));
+router.post('/files', (req, res) =>  FilesController.postUpload(req, res));
 
 export default router;
